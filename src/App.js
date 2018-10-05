29666f092ac9a12bea26f540ba79d617ffc58132
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-05 16:25:00 -0600

    Cycling through a array using map and outputting the list with a unique id

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import Jedi from './components/Jedi.js';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        jedi: [
+          {name: 'Yoda', age: '400', saber: 'Green'},
+          {name: 'Mace', age: '200', saber: 'Purple'},
+          {name: 'Obi', age: '100', saber: 'Blue'},
+          {name: 'Vader', age: '200', saber: 'Red'}
+        ]
+      };
+    }
+
   render() {
     return (
       <div className="App">
         <h1>React Time</h1>
-        <Jedi name='Yoda' age='200' saber='green'/>
-        <Jedi name='Obi' age='82' saber='blue'/>
+        <Jedi jedi={this.state.jedi}/>
       </div>
     );
   }
--- src/components/Jedi.js
+++ src/components/Jedi.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 class Jedi extends Component {
     render() {
-        const { name, age, saber } = this.props;
-        return(
-            <div className="jedi">
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Saber: {saber}</div>
+        const { jedi } = this.props;
+        console.log(jedi, "Jedi Array from parent");
+        const jediList = jedi.map(function(jedi,index){
+            return (  
+            <div className="jedi" key={index}>
+                <div>Name: {jedi.name}</div>
+                <div>Age: {jedi.age}</div>
+                <div>Saber: {jedi.saber}</div>
             </div>
-        );
-    }
+
+            );
+        });
+        // Return Body
+        return(
+            <div>
+                <div className="jedi-list">
+                    {jediList}
+                </div>
+            </div>
+        )
+            
+         
+    };
 }
 
 export default Jedi;
