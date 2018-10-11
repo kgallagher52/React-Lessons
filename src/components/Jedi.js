b935463d6da51d4a53bd9fb628638a43c951c441
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-10 18:55:42 -0600

    Using If Statment for conditional data

--- src/components/Jedi.js
+++ src/components/Jedi.js
@@ -1,25 +1,29 @@
 import React from 'react';
 
 const Jedi = ({jedi}) => {
-    console.log(jedi, "Jedi Array from parent");
     const jediList = jedi.map(function(jedi,index){
-    return (  
-            <div className="jedi" key={index}>
-                <div>Name: {jedi.name}</div>
-                <div>Age: {jedi.age}</div>
-                <div>Saber: {jedi.saber}</div>
-            </div>
+        if ( jedi.age < 250 ) {
+            return (  
+                    <div className="jedi" key={index}>
+                        <div>Name: {jedi.name}</div>
+                        <div>Age: {jedi.age}</div>
+                        <div>Saber: {jedi.saber}</div>
+                    </div>
 
-            );
-        })
+                    );
+        } else {
+            return null;
+        }
+
+    });
         // Return Body
-        return (
-            <div>
-                <div className="jedi-list">
-                    {jediList}
-                </div>
+    return (
+        <div>
+            <div className="jedi-list">
+                {jediList}
             </div>
-        )         
-    }
+        </div>
+    )         
+};
 
 export default Jedi;
