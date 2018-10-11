02e8a4a1b7c7b1e93e5e4ecdb5815c8058f517ae
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-10 19:15:34 -0600

    Using Ternary operator instead of an if statment

--- src/components/Jedi.js
+++ src/components/Jedi.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
 const Jedi = ({jedi}) => {
-    const jediList = jedi.map(function(jedi,index){
-        if ( jedi.age < 250 ) {
-            return (  
-                    <div className="jedi" key={index}>
-                        <div>Name: {jedi.name}</div>
-                        <div>Age: {jedi.age}</div>
-                        <div>Saber: {jedi.saber}</div>
-                    </div>
+    // const jediList = jedi.map(function(jedi,index){
+    //     if ( jedi.age < 250 ) {
+    //         return (  
+    //                 <div className="jedi" key={index}>
+    //                     <div>Name: {jedi.name}</div>
+    //                     <div>Age: {jedi.age}</div>
+    //                     <div>Saber: {jedi.saber}</div>
+    //                 </div>
 
-                    );
-        } else {
-            return null;
-        }
+    //                 );
+    //     } else {
+    //         return null;
+    //     }
 
+    // Ternary Operator 
+        // condition () ? () : ;
+        //  return jedi.age < 250 ? (Stuff to return) : (else return stuff)
+
+    const jediList = jedi.map(jedi => {
+        return jedi.age < 250 ? (
+            <div className="jedi" key={jedi.id}>
+                <div>Name: {jedi.name}</div>
+                <div>Age: {jedi.age}</div>
+                <div>Saber: {jedi.saber}</div>
+            </div> 
+        ) : null;
     });
-        // Return Body
+
     return (
         <div>
             <div className="jedi-list">
