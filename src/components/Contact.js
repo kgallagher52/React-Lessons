2b7e1f806e7cf35f0446cad9c19a3d0aae0951b3
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-11 22:50:33 -0600

    Added a function that used the props history to change the page after two seconds

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const Contact = (props) => {
     console.log(props);
+// This function is using one of the props fields to swithc page to about after Two seconds
+    setTimeout(() => {
+        props.history.push('/about');
+    },2000);
+
     return (
         <div className="container">
             <h4 className="center">Contact</h4>
