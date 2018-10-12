f7d408cf1ff85761d990a24226b53a59b175b833
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-12 00:24:08 -0600

    Added pages for each post when you click title it grabs by the id and redirects you showing the content for only that post according to the id associated with the post but now we have a problem with the posts showing up on other pages

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Post extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id:null
+            post: null
         };
     }
 
     componentDidMount(){
         console.log(this.props);
         let id = this.props.match.params.post_id;
-        this.setState({
-            id: id
+        axios.get('http://jsonplaceholder.typicode.com/posts/' + id).then(res => {
+            this.setState({
+                id: id,
+                post: res.data
+
+            });
         });
+
     }
 
     render() {
+        const post = this.state.post ? (
+            <div className="post">
+                <h4 className="center">{this.state.post.title}</h4>
+                {this.state.post.body}
+            </div>
+        ) : (
+            <div className="center">
+                <h1>Loading Post...</h1>
+            </div>
+        )
+
         return(
             <div className="container">
-                <h4>{this.state.id}</h4>
+                { post }
             </div>
         )
     }
