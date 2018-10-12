b6817ac86a36b5bc56c3e0eeadc960493b9e8a7f
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-12 00:44:06 -0600

    Added images and a function that randomly selects an image so I can make all the posts have different images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import yoda from '../assets/images/Yoda.png';
+import anotherJedi from '../assets/images/New.png';
+import darth from '../assets/images/Darth-Vader.png';
 
 class Home extends Component {
     constructor(props){
@@ -24,10 +27,12 @@ class Home extends Component {
 
     render(){
         const { posts } = this.state;
+        const images = [darth, anotherJedi,yoda];
         const postList = posts.length ? ( 
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
+                        <img src={images[Math.floor(Math.random()*images.length)]} alt="image"/>
                         <div className="card-content">
                             <NavLink to={'/' + post.id}><span className="card-title">{post.title}</span></NavLink>
                             {post.body}
