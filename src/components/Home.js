5f5300e07fe4a743ec40ca79c43dbc548bc0d41b
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-11 23:46:59 -0600

    After collected data displaying it to the screen and using it dynamically

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,45 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: [ ]
+        };
+    }
+
     // This is a great lifecycle to grab data
     componentDidMount() {
         // This is asyncrynos 
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(res => {
                 console.log(res);
+                this.setState({
+                    posts: res.data.slice(0,10)
+                });
             });
     }   
 
     render(){
-
+        const { posts } = this.state;
+        const postList = posts.length ? ( 
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No Posts Yet</div>
+        )
         return (
             <div className="container">
                 <h4 className="center">Home</h4>
-                <p>lorem ipsum is the friend I like to have</p>
+                <p>{postList}</p>
             </div>
         )
     }
