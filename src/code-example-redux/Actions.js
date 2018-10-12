941f50d253095a25994b926a6f60d23bf3b01e42
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-10-12 01:50:12 -0600

    added subscriptions and added a spread operator to get the whole current state so that things don't get deleted

--- src/code-example-redux/Actions.js
+++ src/code-example-redux/Actions.js
@@ -10,13 +10,27 @@ const initState = {
 // Create the reducer function that will be passed into the store
 function myReducer(state = initState, action){   
     // In function is where we do the interacting with the store
- 
+    // Changing the state
+    if (action.type == 'ADD_TODO' ) {
+        return {
+            // New state of the store being returned
+            ...state, //This is a spread operator that grabs the whole state but only changes one that is specified
+            todos: [...state.todos, action.todo]
+            // We are adding to the state like this so we do not edit the state directly
+        }
+    }
+
 
 }
 
 
 // Create the store
 const store = createStore(myReducer);
+// Setting Up subscriptions the things that will listen for changes from the store
+store.subscribe(() => {
+    console.log('state updated');
+    console.log('Current state', store.getState());
+})
 
 
 
